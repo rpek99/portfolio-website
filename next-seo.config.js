a4c46feda460f5d0c029ada6fe64ba155c35101d
Rüstem <rstmpk28@gmail.com>
2022-10-04 14:36:07 +0300

    seo configurations made

---
+++ next-seo.config.js
@@ -0,0 +1,30 @@
+export default {
+    titleTemplate: "Rustem Pek - Full Stack Developer | %s",
+    defaultTitle: "Rustem Pek - Full Stack Developer",
+    description:
+      "İstanbul, Turkiye • Full-Stack Developer • Web3",
+    canonical: "https://www.rustempek.com/",
+    openGraph: {
+      type: "website",
+      locale: "en",
+      url: "https://www.rustempek.com",
+      title: "Rustem Pek - Full Stack Developer",
+      description:
+        "Full-Stack Developer • Web3 • Solidity • Smart Contracts",
+      site_name: "rustempek.com",
+      imageWidth: 1200,
+      imageHeight: 1200,
+    },
+    twitter: {
+      handle: "@rustempk",
+      site: "@rustempk",
+      cardType: "summary_large_image",
+    },
+    additionalMetaTags: [
+      {
+        name: "keywords",
+        content:
+          "developer, engineer, blockchain, solidity, smart contracts, web3, full stack, full stack developer",
+      },
+    ],
+  };
